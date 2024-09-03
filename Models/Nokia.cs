7c71a880ace42a78e90d99f265b13b8b748a8a94
Hermínio Da Silva André Francisco <eryyoficial@gmail.com>
2024-09-03 16:52:10 +0100

    RESOLVIDO COM SUCESSO

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,16 @@
+
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia (string Numero, string Modelo, string IMEI, int Memoria) : base(Numero, Modelo, IMEI, Memoria){
+
+        }
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nókia");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nSmartphone - Nókia");
+Smartphone nok = new Nokia("112233","Modelo 1","11111111",64);
+nok.Ligar();
+nok.InstalarAplicativo("Angry Bird");
+
+Console.WriteLine("\nSmartphone - IPhone");
+Smartphone ip = new Iphone("332211","Modelo 2","1111111",123);
+ip.ReceberLigacao();
+ip.InstalarAplicativo("App Store");
